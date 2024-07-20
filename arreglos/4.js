6c02e08fc1b6035dc5d70832b349be50e5d74db5
JChigne <lemniscatachigne@gmail.com>
2024-07-20 18:36:02 -0400

    Utilizando el metodo unshift y shift

--- arreglos/4.js
+++ arreglos/4.js
@@ -6,10 +6,11 @@ let myArray = [
     "Leeds",
     "Carlisle",
   ];
-// Metodo push
-  let newLength = myArray.push('Bristol');
-// Metodo pop
-  let removeItem = myArray.pop();
-
-
-console.log(removeItem)
+// Metodo push remueve al final
+let newLength = myArray.push('Bristol');
+// Metodo pop elimina el ultimo valor agregado
+let removeItem = myArray.pop();
+//unShift Agrega al principio 
+let newItem = myArray.unshift('Edinburgh')
+//shift Elima el valor al principio
+let removeNewItem = myArray.shift();
