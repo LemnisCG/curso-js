98eda57e34449391b384c679caffd1427ce5dbac
JChigne <lemniscatachigne@gmail.com>
2024-07-20 18:31:53 -0400

    Utilizando el metodo pop

--- arreglos/4.js
+++ arreglos/4.js
@@ -6,6 +6,10 @@ let myArray = [
     "Leeds",
     "Carlisle",
   ];
-
+// Metodo push
   let newLength = myArray.push('Bristol');
-  
+// Metodo pop
+  let removeItem = myArray.pop();
+
+
+console.log(removeItem)
